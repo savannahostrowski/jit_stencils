1eb11fd5ac44f5daee4372163684b857c5f32631
Savannah Ostrowski <savannahostrowski@gmail.com>
2024-12-15 09:41:21 -0800

    cleanup

--- grab_last_30days.py
+++ grab_last_30days.py
@@ -2,22 +2,18 @@ import os
 import requests
 import datetime
 
-# GitHub repository details
 owner = "python"
 repo = "cpython"
-workflow_id = "generate_stencils.yml"  # The name of your workflow file
-token = os.getenv("GITHUB_TOKEN")  # Personal access token with repo scope
+token = os.getenv("GITHUB_TOKEN")
 
 print(token)
 
-# Calculate the date 30 days ago
 thirty_days_ago = (datetime.datetime.now() - datetime.timedelta(days=30)).isoformat()
 
-# Fetch commits from the last 30 days
 commits_url = f"https://api.github.com/repos/{owner}/{repo}/commits"
 params = {
     "since": thirty_days_ago,
-    "per_page": 100  # Adjust as needed
+    "per_page": 100 
 }
 headers = {
     "Authorization": f"token {token}"
@@ -28,16 +24,14 @@ commits = response.json()
 origin_owner = "savannahostrowski"
 origin_repo= "jit_stencils"
 origin_workflow_id = "generate_stencils.yml"
-# Trigger the workflow for each commit
 for commit in commits:
     sha = commit["sha"]
     message = commit["commit"]["message"]
     print(f"Triggering workflow for commit {sha}: {message}")
 
-    # Trigger the workflow_dispatch event
     dispatch_url = f"https://api.github.com/repos/{origin_owner}/{origin_repo}/actions/workflows/{origin_workflow_id}/dispatches"
     data = {
-        "ref": "main",  # The branch to run the workflow on
+        "ref": "main", 
         "inputs": {
             "commit_sha": sha
         }
